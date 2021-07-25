92ac64259bde40a340bec07300c9b39d796715fc
pradeep <pradeepcpk1997@gmail.com>
2021-07-25 17:54:38 +0530

    Add project files.

---
+++ WindowsServiceProject1/SendMailService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace WindowsServiceProject1
+{
+    class SendMailService
+    {
+        // This function write log to LogFile.text when some error occurs.  
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+        // This function write Message to log file.
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+        // This function contains the logic to send mail.
+        public static void SendEmail(String ToEmail, String Subj, string Message)
+        {
+            try
+            {
+                System.Net.Mail.SmtpClient smtpClient = new System.Net.Mail.SmtpClient();
+                smtpClient.EnableSsl = true;
+                smtpClient.Timeout = 200000;
+                MailMessage MailMsg = new MailMessage();
+                System.Net.Mime.ContentType HTMLType = new System.Net.Mime.ContentType("text/html");
+
+                string strBody = "This is a test mail.";
+
+                MailMsg.BodyEncoding = System.Text.Encoding.Default;
+                MailMsg.To.Add(ToEmail);
+                MailMsg.Priority = System.Net.Mail.MailPriority.High;
+                MailMsg.Subject = "Subject - Window Service";
+                MailMsg.Body = strBody;
+                MailMsg.IsBodyHtml = true;
+                AlternateView HTMLView = AlternateView.CreateAlternateViewFromString(strBody, HTMLType);
+
+                smtpClient.Send(MailMsg);
+                WriteErrorLog("Mail sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                WriteErrorLog(ex.Message);
+                //Stop the Windows Service.
+                using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+                throw;
+            }
+        }
+    }
+}
---
+++ WindowsServiceProject1/TestService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Configuration;
+using System.Timers;
+using System.Net;
+using System.Net.Mail;
+
+namespace WindowsServiceProject1
+{
+    public partial class TestService : ServiceBase
+    {
+        private System.Timers.Timer timer1;
+        private string timeString;
+        public int getCallType;
+
+        /////////////////////////////////////////////////////////////////////
+        public TestService()
+        {
+            InitializeComponent();
+
+            //string Msg = "hi";
+            //SendMailService.SendEmail("pradeep.kumar@dotnetethics.com", "Subject", Msg);
+
+            int strTime = Convert.ToInt32(ConfigurationSettings.AppSettings["callDuration"]);
+            getCallType = Convert.ToInt32(ConfigurationSettings.AppSettings["CallType"]);
+            if (getCallType == 1)
+            {
+                timer1 = new System.Timers.Timer();
+                double inter = (double)GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+
+            }
+            else
+            {
+                timer1 = new System.Timers.Timer();
+                timer1.Interval = strTime * 1000;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        protected override void OnStart(string[] args)
+        {
+            timer1.AutoReset = true;
+            timer1.Enabled = true;
+            SendMailService.WriteErrorLog("Service started");
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        protected override void OnStop()
+        {
+            timer1.AutoReset = false;
+            timer1.Enabled = false;
+            SendMailService.WriteErrorLog("Service stopped");
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        private double GetNextInterval()
+        {
+            timeString = ConfigurationSettings.AppSettings["StartTime"];
+            DateTime t = DateTime.Parse(timeString);
+            TimeSpan ts = new TimeSpan();
+            int x;
+            ts = t - System.DateTime.Now;
+            if (ts.TotalMilliseconds < 0)
+            {
+                ts = t.AddDays(1) - System.DateTime.Now;//Here you can increase the timer interval based on your requirments.   
+            }
+            return ts.TotalMilliseconds;
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        private void SetTimer()
+        {
+            try
+            {
+                double inter = (double)GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        private void ServiceTimer_Tick(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string Msg = "Hi ! This is DailyMailSchedulerService mail.";//whatever msg u want to send write here.  
+
+            SendMailService.SendEmail("pradeep.kumar@dotnetethics.com", "Subject", Msg);
+
+            if (getCallType == 1)
+            {
+                timer1.Stop();
+                System.Threading.Thread.Sleep(1000000);  //waiting because we know a specific timespan when we should continue
+                SetTimer();
+            }
+        }
+    }
+}
